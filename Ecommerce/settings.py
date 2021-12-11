1055e91199598b817c7e1d2fda5b8e3778930f39
Niyastk <niyastk9562@gmail.com>
2021-12-11 12:47:01 +0530

    env conf

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from accounts.private import DATA_BASE_CONF, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^7kimq0_o*cede4y_35csexupvr-s!nr=0$#f1v%@5^2qb99%6'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,15 +86,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bluemoon',
-        'USER': 'postgres',
-        'PASSWORD': 'nzx123',
-        'HOST': 'localhost',
-    }
-}
+DATABASES = DATA_BASE_CONF
 
 
 # Password validation
--- accounts/private.py
+++ accounts/private.py
@@ -1,6 +1,15 @@
 account_sid = 'AC8d186daf0cc71c3acdd9180662cb88a1'
-auth_token = 'f8320fe18dcffb4a630b67e3487df973'
+auth_token = '4f26ae2eb091483c6aff0ebe468839f6'
 service = 'VA610d69d175f9665eb59dcb45b5b0600a'
 
+SECRET_KEY = 'django-insecure-^7kimq0_o*cede4y_35csexupvr-s!nr=0$#f1v%@5^2qb99%6'
 
-
+DATA_BASE_CONF = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bluemoon',
+        'USER': 'postgres',
+        'PASSWORD': 'nzx123',
+        'HOST': 'localhost',
+    }
+}
